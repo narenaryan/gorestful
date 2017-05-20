503579e77768749c4cf9c2c64cfbd016a759792a
Naren <naren@Narens-MacBook-Air.local>
2017-05-20 10:14:54 +0530

    minor fix

--- chapter2/multipleHandlers.go
+++ chapter2/multipleHandlers.go
@@ -2,19 +2,19 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
 	"net/http"
-     "math/rand"
 )
 
 func main() {
 	newMux := http.NewServeMux()
 
-newMux.HandleFunc("/randomFloat", func(w http.ResponseWriter, r *http.Request) {
- fmt.Fprintln(w, rand.Float64())
-})
+	newMux.HandleFunc("/randomFloat", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, rand.Float64())
+	})
 
-newMux.HandleFunc("/randomInt", func(w http.ResponseWriter, r *http.Request) {
- fmt.Fprintln(w, rand.Intn(100))
-})
-    http.ListenAndServe(":8000", newMux)
+	newMux.HandleFunc("/randomInt", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, rand.Intn(100))
+	})
+	http.ListenAndServe(":8000", newMux)
 }
