744a3a728036f4973c44949f56e9852ece990275
Naren <narenarya@live.com>
2017-07-16 18:50:37 +0530

    Fixed comments

--- chapter6/grpc_example/grpcServer/server.go
+++ chapter6/grpc_example/grpcServer/server.go
@@ -2,35 +2,40 @@ package main
 
 import (
 	"log"
+	"net"
 
 	pb "github.com/narenaryan/datafiles"
 	"golang.org/x/net/context"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
 )
 
 const (
-	address = "localhost:50051"
+	port = ":50051"
 )
 
-func main() {
-	// Set up a connection to the server.
-	conn, err := grpc.Dial(address, grpc.WithInsecure())
-	if err != nil {
-		log.Fatalf("Did not connect: %v", err)
-	}
-	defer conn.Close()
-	c := pb.NewMoneyTransactionClient(conn)
+// server is used to create MoneyTransactionServer.
+type server struct{}
 
-	// Prepare data. Get this from clients like Frontend or App
-	from := "1234"
-	to := "5678"
-	amount := float32(1250.75)
-
-	// Contact the server and print out its response.
-	r, err := c.MakeTransaction(context.Background(), &pb.TransactionRequest{From: from,
-		To: to, Amount: amount})
-	if err != nil {
-		log.Fatalf("Could not transact: %v", err)
-	}
-	log.Printf("Transaction confirmed: %t", r.Confirmation)
+// MakeTransaction implements MoneyTransactionServer.MakeTransaction
+func (s *server) MakeTransaction(ctx context.Context, in *pb.TransactionRequest) (*pb.TransactionResponse, error) {
+	log.Printf("Got request for money Transfer....")
+	log.Printf("Amount: %f, From A/c:%s, To A/c:%s", in.Amount, in.From, in.To)
+	// Do database logic here....
+	return &pb.TransactionResponse{Confirmation: true}, nil
 }
+
+func main() {
+	lis, err := net.Listen("tcp", port)
+	if err != nil {
+		log.Fatalf("Failed to listen: %v", err)
+	}
+	s := grpc.NewServer()
+	pb.RegisterMoneyTransactionServer(s, &server{})
+	// Register reflection service on gRPC server.
+	reflection.Register(s)
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("Failed to serve: %v", err)
+	}
+}
+
