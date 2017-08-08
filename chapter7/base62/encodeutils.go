97ab59ccd70fbdb3d89946a9beb3ab4900315fd7
Naren <narenarya@live.com>
2017-08-08 23:17:48 +0530

    fixed a bug

--- chapter7/base62/encodeutils.go
+++ chapter7/base62/encodeutils.go
@@ -11,7 +11,7 @@ const b = 62
 // Function encodes the given database ID to a base62 string
 func ToBase62(num int) string{
     r := num % b
-    res := base[r]
+    res := string(base[r])
     div := num / b
     q := int(math.Floor(float64(div)))
 
@@ -19,7 +19,7 @@ func ToBase62(num int) string{
         r = q % b
         temp := q / b
         q = int(math.Floor(float64(temp)))
-        res = base[int(r)] + res
+        res = string(base[int(r)]) + res
     }
 
     return string(res)
