dc0d75ece6033acdc6cc6d121cdab0667a4d5f5c
Naren Yellavula <naren.yellavula@citrix.com>
2017-10-28 16:11:24 +0530

    added dockerfile

---
+++ chapter11/kongExample/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"github.com/gorilla/mux"
+	"log"
+	"net/http"
+	"time"
+)
+
+func HealthcheckHandler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, time.Now().String())
+}
+func main() {
+	// Create a new router
+	r := mux.NewRouter()
+	// Attach an elegant path with handler
+	r.HandleFunc("/healthcheck", HealthcheckHandler)
+	srv := &http.Server{
+		Handler: r,
+		Addr:    "0.0.0.0:3001",
+		// Good practice: enforce timeouts for servers you create!
+		WriteTimeout: 15 * time.Second,
+		ReadTimeout:  15 * time.Second,
+	}
+	log.Fatal(srv.ListenAndServe())
+}
