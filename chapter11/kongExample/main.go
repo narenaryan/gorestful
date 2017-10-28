8e8d892d4390676e012b72f9527988b50688ffd0
Naren Yellavula <naren.yellavula@citrix.com>
2017-10-28 16:19:25 +0530

    minor fix

--- chapter11/kongExample/main.go
+++ chapter11/kongExample/main.go
@@ -2,10 +2,11 @@ package main
 
 import (
 	"fmt"
-	"github.com/gorilla/mux"
 	"log"
 	"net/http"
 	"time"
+
+	"github.com/gorilla/mux"
 )
 
 func HealthcheckHandler(w http.ResponseWriter, r *http.Request) {
@@ -19,7 +20,7 @@ func main() {
 	r.HandleFunc("/healthcheck", HealthcheckHandler)
 	srv := &http.Server{
 		Handler: r,
-		Addr:    "0.0.0.0:3001",
+		Addr:    "0.0.0.0:3000",
 		// Good practice: enforce timeouts for servers you create!
 		WriteTimeout: 15 * time.Second,
 		ReadTimeout:  15 * time.Second,
